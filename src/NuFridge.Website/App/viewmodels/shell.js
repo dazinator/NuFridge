5ea39e978dbd3269a80f4f594e30b3425bf4e64e
lukeskinner <fluffedup2009@gmail.com>
2014-06-05 23:34:57 +0100

    Added durandal starter kit

--- src/NuFridge.Website/App/viewmodels/shell.js
+++ src/NuFridge.Website/App/viewmodels/shell.js
@@ -1,16 +1,17 @@
-﻿define(['plugins/router'], function (router) {
+﻿define(['plugins/router', 'durandal/app'], function (router, app) {
     return {
         router: router,
+        search: function () {
+            //It's really easy to show a message box.
+            //You can add custom options too. Also, it returns a promise for the user's response.
+            app.showMessage('Search not yet implemented...');
+        },
         activate: function () {
             router.map([
-           { route: '', moduleId: 'viewmodels/signin/signin' },
-                { route: 'home', moduleId: 'viewmodels/signin/signin', nav: true }
-                //{ route: 'tickets', moduleId: 'tickets/index', nav: true },
-                //{ route: 'tickets/:id', moduleId: 'tickets/thread' },
-                //{ route: 'users(/:id)', moduleId: 'users/index', hash: '#users', nav: true },
-                //{ route: 'settings*details', moduleId: 'settings/index', hash: '#settings', nav: true }
+                { route: '', title: 'Welcome', moduleId: 'viewmodels/signin/signin', nav: true },
+                { route: 'flickr', moduleId: 'viewmodels/flickr', nav: true }
             ]).buildNavigationModel();
-       
+
             return router.activate();
         }
     };
