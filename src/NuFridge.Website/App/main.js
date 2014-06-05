2763c702f1fdad9b829927177d217cd2e48c098f
lukeskinner <fluffedup2009@gmail.com>
2014-06-05 23:21:45 +0100

    Add durandal to the solution. Add test index.html page.

---
+++ src/NuFridge.Website/App/main.js
@@ -0,0 +1,26 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../Scripts/text',
+        'durandal': '../Scripts/durandal',
+        'plugins': '../Scripts/durandal/plugins',
+        'transitions': '../Scripts/durandal/transitions',
+        'knockout': '../Scripts/knockout-2.3.0',
+        'jquery': '../Scripts/jquery-1.9.1',
+    }
+});
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'plugins/router'], function (system, app, viewLocator, router) {
+
+    system.debug(true);
+  
+    app.title = 'NuFridge';
+
+    app.configurePlugins({
+        router: true
+    });
+
+    app.start().then(function () {
+        viewLocator.useConvention();
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
+});
---
+++ src/NuFridge.Website/App/viewmodels/shell.js
@@ -0,0 +1,17 @@
+﻿define(['plugins/router'], function (router) {
+    return {
+        router: router,
+        activate: function () {
+            router.map([
+           { route: '', moduleId: 'viewmodels/signin/signin' },
+                { route: 'home', moduleId: 'viewmodels/signin/signin', nav: true }
+                //{ route: 'tickets', moduleId: 'tickets/index', nav: true },
+                //{ route: 'tickets/:id', moduleId: 'tickets/thread' },
+                //{ route: 'users(/:id)', moduleId: 'users/index', hash: '#users', nav: true },
+                //{ route: 'settings*details', moduleId: 'settings/index', hash: '#settings', nav: true }
+            ]).buildNavigationModel();
+       
+            return router.activate();
+        }
+    };
+});
